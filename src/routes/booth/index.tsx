47749ab6b9d6a28cb93f8614ecbfcb40062c9957
mzxxzysy <jsyjj21@gmail.com>
2025-11-20 11:33:14 +0900

    ✨ Feat: 부스 배치도 퍼블리싱 (#11)

--- src/routes/booth/index.tsx
+++ src/routes/booth/index.tsx
@@ -1,9 +1,11 @@
 import BoothMap from "@/components/booth/BoothMap";
+import StudioList from "@/components/booth/StudioList";
 
 const BoothPage = () => {
   return (
     <main className="flex flex-col justify-center px-3">
       <BoothMap />
+      <StudioList />
     </main>
   );
 };
