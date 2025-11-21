207b7cf3c4ec3cc679f86603b364f5a03851d4b8
jinhyo <kjhyo0220@gmail.com>
2025-11-21 14:59:26 +0900

    ✨ Feat: 오시는 길 퍼블리싱 (#10)

--- src/routes/exhibition/components/Way.tsx
+++ src/routes/exhibition/components/Way.tsx
@@ -1,7 +1,28 @@
 import { useEffect } from "react";
 import Layout from "./Layout";
 
+import SubwayIcon from "@/assets/subwayIcon.svg?react";
+import BusIcon from "@/assets/busIcon.svg?react";
+
 const Way = () => {
+  const TRANSPORTATION = [
+    {
+      id: 1,
+      icon: <SubwayIcon className="shrink-0" />,
+      text: <p>1호선, 4호선 창동역</p>,
+    },
+    {
+      id: 2,
+      icon: <BusIcon className="shrink-0" />,
+      text: (
+        <div>
+          <p>1119, 1120, 1129, 1142</p>
+          <p>노원 05, 노원 08, 노원 15, 도봉 01, 도봉 08, 도봉 09</p>
+        </div>
+      ),
+    },
+  ];
+
   useEffect(() => {
     const script = document.createElement("script");
     script.async = false;
@@ -39,13 +60,22 @@ const Way = () => {
     <Layout
       title="오시는 길"
       content={
-        <div className="flex flex-col gap-[24px]">
-          <div>
+        <div className="flex flex-col gap-[24px] text-s-medium leading-[19.6px]">
+          <div className=" text-grey-normal text-center">
             <p>서울 도봉구 마들로 13길 84</p>
             <p>서울창업허브 창동 B1</p>
           </div>
 
           <div id="map" className="w-full h-[257px]"></div>
+
+          <div className="flex flex-col gap-[16px]">
+            {TRANSPORTATION.map((t) => (
+              <div key={t.id} className="flex gap-[24px] text-blue-lightHover">
+                {t.icon}
+                {t.text}
+              </div>
+            ))}
+          </div>
         </div>
       }
     />
