65099bd856073f47505d2dadf2b0851919b86089
jinhyo <kjhyo0220@gmail.com>
2025-11-22 14:31:03 +0900

    ✨ Feat: 전시 소개 애니메이션 추가 (#10)

--- src/routes/exhibition/index.tsx
+++ src/routes/exhibition/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Intro from "./components/Intro";
 import Divider from "./components/Divider";
 import Schedule from "./components/Schedule";
@@ -9,14 +11,132 @@ import mainBG from "@/assets/main.png";
 import LogoIcon from "@/assets/rendering-logo.png";
 import LineIcon from "@/assets/lineIcon.svg";
 import LineOrangeIcon from "@/assets/LineOrangeIcon.svg";
+import Left1 from "@/assets/animation-left1-fruit.svg?react";
+import Left2 from "@/assets/animation-left2-art.svg?react";
+import Left3 from "@/assets/animation-left3-text.svg?react";
+import Left4 from "@/assets/animation-left4.svg?react";
+import Left5 from "@/assets/animation-left5-art.svg?react";
+import Left6 from "@/assets/animation-left6-text.svg?react";
+import Left7 from "@/assets/animation-left7-line.svg?react";
+import Right1 from "@/assets/animation-right1-art.svg?react";
+import Right2 from "@/assets/animation-right2-card.svg?react";
+import Right3 from "@/assets/animation-right3-sound.svg?react";
+import Right4 from "@/assets/animation-right4-line.svg?react";
 
 const ExhibitionPage = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    if (!isClicked) setIsClicked(true);
+  };
+
   return (
     <>
-      <section className="relative z-20 flex flex-col items-center">
-        <div className="max-w-[300px] w-full mt-[50px] mb-[78px]">
-          <img src={LogoIcon} alt="rendering" className="w-full h-full object-contain" />
+      <section
+        onClick={handleClick}
+        className="relative z-20 flex flex-col items-center "
+      >
+        <div
+          className={`${
+            isClicked ? "pt-[71px]" : "pt-[50px]"
+          } h-[175px] transition-all duration-700 ease-out`}
+        >
+          <div
+            className={`${
+              isClicked ? "max-w-[254px]" : "max-w-[272px]"
+            } w-full duration-700 ease-out`}
+          >
+            <img
+              src={LogoIcon}
+              alt="rendering"
+              className="w-full h-full object-contain"
+            />
+          </div>
         </div>
+
+        <Left1
+          className={`absolute top-[70px] left-[-24px] transition-all duration-700 ease-out transform ${
+            isClicked
+              ? "opacity-100 translate-x-0 translate-y-0"
+              : "opacity-0 -translate-x-20 -translate-y-20"
+          }`}
+        />
+
+        <Left2
+          className={`absolute top-[340px] left-[-24px] transition-all duration-700 ease-out transform ${
+            isClicked
+              ? "opacity-100 translate-x-0 translate-y-0"
+              : "opacity-0 -translate-x-16"
+          }`}
+        />
+
+        <Left3
+          className={`absolute top-[633px] left-[-24px] transition-all duration-700 ease-out transform ${
+            isClicked
+              ? "opacity-100 translate-x-0 translate-y-0"
+              : "opacity-0 -translate-x-full"
+          }`}
+        />
+
+        <Left4
+          className={`absolute top-[615px] left-[-24px] transition-all duration-700 ease-out transform ${
+            isClicked
+              ? "opacity-100 translate-x-0 translate-y-0"
+              : "opacity-0 -translate-x-full translate-y-40"
+          }`}
+        />
+
+        <Left5
+          className={`absolute top-[1002px] left-[24px] transition-all duration-700 ease-out transform ${
+            isClicked
+              ? "opacity-100 translate-x-0 translate-y-0"
+              : "opacity-0 -translate-x-full translate-y-40"
+          }`}
+        />
+
+        <Left6
+          className={`absolute top-[1063px] left-[-24px] transition-all duration-700 ease-out transform ${
+            isClicked
+              ? "opacity-100 translate-x-0 translate-y-0"
+              : "opacity-0 -translate-x-full translate-y-40"
+          }`}
+        />
+
+        <Left7
+          className={`absolute top-[1239px] left-[-24px] transition-all duration-700 ease-out transform ${
+            isClicked ? "opacity-100" : "opacity-0"
+          }`}
+        />
+
+        <Right1
+          className={`absolute top-[131px] right-[-24px] transition-all duration-700 ease-out transform ${
+            isClicked
+              ? "opacity-100 translate-x-0 translate-y-0"
+              : "opacity-0 translate-x-16 -translate-y-16"
+          }`}
+        />
+
+        <Right2
+          className={`absolute top-[434px] right-[-24px] transition-all duration-700 ease-out transform ${
+            isClicked
+              ? "opacity-100 translate-x-0 translate-y-0"
+              : "opacity-0 translate-x-28"
+          }`}
+        />
+
+        <Right3
+          className={`absolute top-[777px] right-[-24px] transition-all duration-700 ease-out transform ${
+            isClicked
+              ? "opacity-100 translate-x-0 translate-y-0"
+              : "opacity-0 translate-x-full"
+          }`}
+        />
+
+        <Right4
+          className={`absolute top-[1592px] right-[-24px] transition-all duration-700 ease-out transform ${
+            isClicked ? "opacity-100" : "opacity-0 "
+          }`}
+        />
 
         <Intro />
 
