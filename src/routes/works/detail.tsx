58e91c8d614b6f32728de88000db659ef90a0e86
seunghyeonKang <number42isuser@gmail.com>
2025-11-23 06:17:41 +0900

    💄 Design : 작품목록 페이지 UI 디자인

--- src/routes/works/detail.tsx
+++ src/routes/works/detail.tsx
@@ -1,19 +1,12 @@
 import { useParams } from "react-router-dom";
-import { PROJECT_LIST } from "@/data/projectListData";
+import WorksDetailContents from "./components/works-detail/WorksDetailContents";
 
 const WorksDatailPage = () => {
   const { id } = useParams<{ id: string }>();
-  const work = PROJECT_LIST.find((item) => String(item.id) === id);
-
-  if (!work) {
-    return <div className="">존재하지 않는 작품입니다.</div>;
-  }
 
   return (
-    <section className="grid grid-cols-2 gap-6">
-      <p>| {work.teamName}</p>
-      <h1>{work.title}</h1>
-      <p>{work.oneLineDescription}</p>
+    <section className="w-full">
+      <WorksDetailContents id={Number(id)} />
     </section>
   );
 };
