9689f30dad53297abc0d54f5411fa564ee882342
seunghyeonKang <number42isuser@gmail.com>
2025-11-23 09:48:06 +0900

    ✨ Feat: 작품설명 페이지 - 페이지네이션 추가

---
+++ src/routes/works/components/works-detail/Pagination.tsx
@@ -0,0 +1,47 @@
+import { useNavigate } from "react-router-dom";
+import NextIcon from "@/assets/nextPoly.svg?react";
+import PrevIcon from "@/assets/prevPoly.svg?react";
+
+interface PaginationProps {
+  id: number;
+}
+
+const Pagination = ({ id }: PaginationProps) => {
+  const navigate = useNavigate();
+
+  const handleMoveToPrevPage = () => {
+    if (id < 2) {
+      navigate(`/works/23`);
+    } else {
+      navigate(`/works/${id - 1}`);
+    }
+  };
+  const handleMoveToNextPage = () => {
+    if (id > 22) {
+      navigate(`/works/1`);
+    } else {
+      navigate(`/works/${id + 1}`);
+    }
+  };
+
+  return (
+    <div className="flex justify-between mt-[-30px] mb-[36px]">
+      <button
+        className="flex items-center py-[12px] pr-[12px] gap-[6px]"
+        onClick={handleMoveToPrevPage}
+      >
+        <PrevIcon className="w-[12px] h-[12px]" />
+        <p className="text-[12px] text-text-m-bold text-red-normal50">이전</p>
+      </button>
+      <button
+        className="flex items-center py-[12px] pl-[12px] gap-[6px]"
+        onClick={handleMoveToNextPage}
+      >
+        <p className="text-[12px] text-text-m-bold text-red-normal50">다음</p>
+        <NextIcon className="w-[12px] h-[12px]" />
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
--- src/routes/works/detail.tsx
+++ src/routes/works/detail.tsx
@@ -1,12 +1,14 @@
 import { useParams } from "react-router-dom";
 import WorksDetailContents from "./components/works-detail/WorksDetailContents";
+import Pagination from "./components/works-detail/Pagination";
 
 const WorksDatailPage = () => {
   const { id } = useParams<{ id: string }>();
 
   return (
-    <section className="w-full">
+    <section className="w-full mt-[36px]">
       <WorksDetailContents id={Number(id)} />
+      <Pagination id={Number(id)} />
     </section>
   );
 };
