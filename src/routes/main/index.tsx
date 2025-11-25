4081662a2dc40dfd967be14ed5080ebe4b426aa1
jinhyo <kjhyo0220@gmail.com>
2025-11-25 16:06:34 +0900

    ♻️ Refactor: 중복되는 디자인 코드 삭제 (#26)

--- src/routes/main/index.tsx
+++ src/routes/main/index.tsx
@@ -22,70 +22,33 @@ const MainPage = () => {
 
   return (
     <>
-      {/* 모바일 레이아웃 */}
-      <section className="md:hidden max-w-[480px] w-full m-auto relative z-20 flex flex-col items-center justify-center h-dvh pt-[125px] pb-[35px]">
-        <div className="flex flex-col items-end">
-          <h1 className="text-blue-light text-[14px] max-[380px]:text-[12px] font-extrabold text-right">
-            2025 덕성여자대학교 IT미디어공학전공 제13회 졸업전시회
-          </h1>
-          <div className="max-w-[315px] w-[73vw] mt-[18px] mb-[10px]">
-            <img src={Logo} alt="RENDERING" className="w-full h-full object-cover" />
-          </div>
-          <h3 className="text-blue-lightActive text-right text-[17px] font-bold tracking-[5.1px]">
-            상상이 현실이 되는 순간
-          </h3>
-        </div>
-
-        <div className="w-screen max-w-[480px] mr-[11px]">
-          <img src={HomeIcon} alt="icon" className="w-full h-full object-contain" />
-        </div>
-
-        <img src={TextIcon} className="fixed top-[69vh] left-[35px] z-[-1]" />
-        <img src={CodeIcon} className="fixed top-[77vh] left-[35px] z-[-1]" />
-
-        <div className="flex flex-col gap-[20px] items-center w-full mt-[5vh]">
-          {MENU_ROWS.map((row, idx) => (
-            <div key={idx} className={`${row.gap} flex justify-center w-full`}>
-              {row.items.map((menu) => (
-                <NavLink
-                  key={menu.id}
-                  to={menu.to}
-                  className="max-w-[100px] w-full h-[36px] rounded-[4px] bg-red-normal flex justify-center items-center text-xs-medium leading-[16px] text-grey-normal"
-                >
-                  {menu.label}
-                </NavLink>
-              ))}
-            </div>
-          ))}
-        </div>
-      </section>
-
-      {/* 데스크탑 레이아웃 */}
-      <section className="hidden md:flex md:flex-col md:items-center md:justify-center md:relative md:z-10 md:w-full md:min-h-[932px]">
-        {/* 데스크탑 배경 */}
+      <section className="max-md:max-w-[480px] w-full max-md:m-auto relative z-20 md:z-10 md:min-h-[932px] md:z-10 flex flex-col items-center justify-center h-dvh">
         <div
-          className="hidden md:block md:absolute md:inset-0 md:z-[1] md:bg-cover md:bg-center"
+          className="max-md:fixed absolute inset-0 z-0 bg-cover bg-center  h-full w-full md:z-[1]"
           style={{ backgroundImage: `url(${HomeBG})` }}
         ></div>
 
-        <div className="w-full flex flex-col items-center gap-6 relative z-10">
-          <h1 className="text-blue-light text-[14px] font-extrabold text-center">
-            2025 덕성여자대학교 IT미디어공학전공 제13회 졸업전시회
-          </h1>
-
-          <div className="w-full max-w-[315px]">
-            <img src={Logo} alt="RENDERING" className="w-full h-full object-cover" />
+        <div className="md:w-full md:flex md:flex-col md:items-center md:gap-6 relative z-10">
+          <div className="flex flex-col items-end">
+            <h1 className="text-blue-light text-[14px] max-[380px]:text-[12px] font-extrabold text-right">
+              2025 덕성여자대학교 IT미디어공학전공 제13회 졸업전시회
+            </h1>
+            <div className="max-w-[315px] w-[73vw] mt-[18px] mb-[10px]">
+              <img src={Logo} alt="RENDERING" className="w-full h-full object-cover" />
+            </div>
+            <h3 className="text-blue-lightActive text-right text-[17px] font-bold tracking-[5.1px]">
+              상상이 현실이 되는 순간
+            </h3>
           </div>
 
-          <h3 className="text-blue-lightActive text-center text-[17px] font-bold tracking-[5.1px]">
-            상상이 현실이 되는 순간
-          </h3>
-
-          <div className="w-full max-w-[315px] mt-4">
+          <div className="w-screen max-w-[480px] mr-[11px]">
             <img src={HomeIcon} alt="icon" className="w-full h-full object-contain" />
           </div>
 
-          <div className="flex flex-col gap-[20px] items-center w-full mt-8">
+          <img src={TextIcon} className="absolute top-[69vh] left-[35px] z-[-1]" />
+          <img src={CodeIcon} className="absolute top-[77vh] left-[35px] z-[-1]" />
+
+          <div className="flex flex-col gap-[20px] items-center w-full mt-[5vh]">
             {MENU_ROWS.map((row, idx) => (
               <div key={idx} className={`${row.gap} flex justify-center w-full`}>
                 {row.items.map((menu) => (
@@ -102,12 +65,6 @@ const MainPage = () => {
           </div>
         </div>
       </section>
-
-      {/* 모바일 배경 */}
-      <div
-        className="md:hidden fixed inset-0 bg-cover bg-center z-10 h-dvh"
-        style={{ backgroundImage: `url(${HomeBG})` }}
-      ></div>
     </>
   );
 };
