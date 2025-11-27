16471dd8cbb854a4f36620546955e63e4b65e5f9
jinhyo <kjhyo0220@gmail.com>
2025-11-27 20:09:36 +0900

    💄 Design: 높이 수정 (#26)

--- src/routes/main/index.tsx
+++ src/routes/main/index.tsx
@@ -19,7 +19,7 @@ const MainPage = () => {
 
   return (
     <>
-      <section className="max-md:max-w-[480px] w-full max-md:m-auto relative z-20 md:z-10 md:h-[932px] md:z-10 flex justify-center items-center max-md:h-dvh h-full overflow-y-hidden">
+      <section className="max-md:max-w-[480px] w-full max-md:m-auto relative z-20 md:z-10 md:z-10 flex justify-center items-center max-md:h-dvh h-full">
         <div
           className="max-md:fixed absolute inset-0 z-0 bg-cover bg-center h-dvh w-full md:z-[1]"
           style={{ backgroundImage: `url(${HomeBG})` }}
